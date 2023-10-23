9f9bd855d7795408be71b0b3c211fce86d4810ee
Trần Quang Trí <tranwangtristudy@gmail.com>
2023-10-24 00:07:31 +0700

    Done

---
+++ ReactJS/BC54_TranQuangTri_ReactJS_Buoi1/reactjs-buoi1/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./BaiTapThucHanhLayout/Header";
+import Body from "./BaiTapThucHanhLayout/Body";
+import Footer from "./BaiTapThucHanhLayout/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
